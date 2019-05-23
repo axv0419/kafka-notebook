0660b1ebd2a2a8a7fe201b7e9dd0ed54b206a755
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-23 10:10:04 -0700

    scripts

---
+++ jovyan/producer.py
@@ -0,0 +1,47 @@
+import datetime
+import time
+import sys
+import yaml
+import json
+
+from confluent_kafka import Producer,Consumer,KafkaError
+from confluent_kafka import avro
+from confluent_kafka.avro import AvroProducer
+
+from collections import defaultdict
+import argparse
+import traceback
+
+
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered {} {} {} [{}]'.format( msg.timestamp(),msg.offset(), msg.topic(), msg.partition()))
+
+
+def read_and_feed_file(conf,topic,filepath):
+    producer = Producer(conf)
+    with open(filepath,"r") as f:
+        data = json.load(f)
+    
+    key = data['payload']['Account_Id__c']
+    
+    producer.produce(topic, data, key=key, callback=delivery_report,headers={"src":"poc-script"})
+    try:
+        producer.poll(0)
+    except:
+        traceback.print_exc()
+
+    producer.flush()
+
+
+
+
+
+if __name__ == "__main__":
+    from cc_config_eu import conf
+    read_and_feed_file(conf,sys.argv[1],sys.argv[2])
