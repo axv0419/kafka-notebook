839e121f71bf240cf6ec9fadb305e61e8f98bb54
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-05-23 10:21:12 -0700

    scripts

--- jovyan/cc_config_eu.py
+++ jovyan/cc_config_eu.py
@@ -5,7 +5,7 @@ conf = {
     'security.protocol': 'SASL_SSL',
     'sasl.mechanisms': 'PLAIN',
     'sasl.username': 'LIKDU2ITTTSFTFPP',
-    'debug':'all',
+    # 'debug':'all',
     'sasl.password': '1Qk2A6AJ9aD3OMHYTFwJ96WvyX3ZnpnUvKwwVKqpKsWlkNWq7wV81YQ/JubNt+B2',
     # 'schema.registry.url': 'http://35.246.229.124:8085',
 }
